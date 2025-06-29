ec64aedb43874f6a2847bd6e9893b0354940562c
Stas Sergeev <stsp2@yandex.ru>
2025-06-29 10:54:52 +0300

    implement socket()

--- accept.c
+++ accept.c
@@ -33,6 +33,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
+#include "csock.h"
 #if defined( __LINUX__ )
 #include "linuxsys.h"
 #elif defined( __RDOS__ )
--- socket.c
+++ socket.c
@@ -1,74 +1,60 @@
-/****************************************************************************
-*
-*                            Open Watcom Project
-*
-* Copyright (c) 2015-2016 The Open Watcom Contributors. All Rights Reserved.
-*    Portions Copyright (c) 1983-2002 Sybase, Inc. All Rights Reserved.
-*
-*  ========================================================================
-*
-*    This file contains Original Code and/or Modifications of Original
-*    Code as defined in and that are subject to the Sybase Open Watcom
-*    Public License version 1.0 (the 'License'). You may not use this file
-*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
-*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
-*    provided with the Original Code and Modifications, and is also
-*    available at www.sybase.com/developer/opensource.
-*
-*    The Original Code and all software distributed under the License are
-*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
-*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-*    NON-INFRINGEMENT. Please see the License for the specific language
-*    governing rights and limitations under the License.
-*
-*  ========================================================================
-*
-* Description:  Implementation of socket() for Linux and RDOS.
-*
-****************************************************************************/
-
+/* socket lib by @stsp */
+/* Sybase Open Watcom Public License */
 
 #include "variety.h"
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include "csock.h"
 
-#if defined( __LINUX__ )
-#include "linuxsys.h"
-#elif defined( __RDOS__ )
-#include "rdos.h"
-#endif
+static int initialized;
+struct driver_info_rec driver_info;
+
+static int socket_init(void)
+{
+    const int CSOCK_VXD_ID = 0x1235;
+    unsigned int __csock_version = 0;
+    int ret = VxdGetEntry (/*csock_entry, */CSOCK_VXD_ID);
+    if (ret < 0)
+        return ret;
+
+    ret = ___csock_get_version(&__csock_version, sizeof(driver_info),
+            &driver_info);
+    if (ret < 0 || __csock_version < 2)
+        return -1;
+    return 0;
+}
 
 _WCRTLINK int socket( int domain, int type, int protocol )
 {
-#if defined( __LINUX__ )
-    unsigned long args[3];
+    int err = 0;
+    int proto;
+    unsigned int fd;
 
-    args[0] = (unsigned long)domain;
-    args[1] = (unsigned long)type;
-    args[2] = (unsigned long)protocol;
-    return( __socketcall( SYS_SOCKET, args ) );
-#elif defined( __RDOS__ )
-
-    /* unused parameters */ (void)protocol;
-
-    if( domain == AF_INET ) {
-        switch( type ) {
+    if (!initialized) {
+        err = socket_init();
+        initialized++;
+    }
+    if (err)
+        return err;
+    if (domain != AF_INET)
+        return -1;
+    if (protocol == 0) {
+        switch (type) {
         case SOCK_STREAM:
-            return( RdosCreateTcpSocket() );
+            proto = IPPROTO_TCP;
+            break;
         case SOCK_DGRAM:
-            return( RdosCreateUdpSocket() );
+            proto = IPPROTO_UDP;
+            break;
         default:
-            return( -1 );
+            return -1;
         }
     } else {
-        return( -1 );
+        proto = protocol;
     }
-#else
-
-    /* unused parameters */ (void)domain; (void)type; (void)protocol;
-
-    return( -1 );
-#endif
+    err = ___csock_open(proto, &fd);
+    if (err)
+        return -1;
+    return fd;
 }
