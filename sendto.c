6c021b8ce1feba6a6d77530f505ebe045b6b949e
Stas Sergeev <stsp2@yandex.ru>
2025-06-29 14:51:23 +0300

    sendto

--- sendto.c
+++ sendto.c
@@ -1,66 +1,19 @@
-/****************************************************************************
-*
-*                            Open Watcom Project
-*
-* Copyright (c) 2008-2016 The Open Watcom Contributors. All Rights Reserved.
-*
-*  ========================================================================
-*
-*    This file contains Original Code and/or Modifications of Original
-*    Code as defined in and that are subject to the Sybase Open Watcom
-*    Public License version 1.0 (the 'License'). You may not use this file
-*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
-*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
-*    provided with the Original Code and Modifications, and is also
-*    available at www.sybase.com/developer/opensource.
-*
-*    The Original Code and all software distributed under the License are
-*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
-*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-*    NON-INFRINGEMENT. Please see the License for the specific language
-*    governing rights and limitations under the License.
-*
-*  ========================================================================
-*
-* Description:  Implementation of sendto() for Linux and RDOS.
-*
-****************************************************************************/
-
+/* socket lib by @stsp */
+/* Sybase Open Watcom Public License */
 
 #include "variety.h"
 #include <sys/types.h>
 #include <sys/socket.h>
-
-#if defined( __LINUX__ )
-#include "linuxsys.h"
-#elif defined( __RDOS__ )
-#include "rdos.h"
-#endif
+#include <netinet/in.h>
+#include "csock.h"
 
 _WCRTLINK int sendto( int s, const void *msg, size_t len, int flags, const struct sockaddr *to, socklen_t tolen )
 {
-#if defined( __LINUX__ )
-    unsigned long args[6];
-
-    args[0] = (unsigned long)s;
-    args[1] = (unsigned long)msg;
-    args[2] = (unsigned long)len;
-    args[3] = (unsigned long)flags;
-    args[4] = (unsigned long)to;
-    args[5] = (unsigned long)tolen;
-    return( __socketcall( SYS_SENDTO, args ) );
-#elif defined( __RDOS__ )
-
-    /* unused parameters */ (void)s; (void)msg; (void)len; (void)flags; (void)to; (void)tolen;
-
-    return( -1 );
-#else
-
-    /* unused parameters */ (void)s; (void)msg; (void)len; (void)flags; (void)to; (void)tolen;
-
-    return( -1 );
-#endif
+    ULONG32 sentlen;
+    struct sockaddr_in *to_sa = (struct sockaddr_in *) to;
+    int ret = ___csock_sendto(s, to_sa->sin_addr.s_addr,
+            to_sa->sin_port, msg, len, &sentlen);
+    if (ret < 0)
+        return -1;
+    return sentlen;
 }
-
