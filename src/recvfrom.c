26855dbeedb0ccd6fbd5f432048057aa481d8eae
Stas Sergeev <stsp2@yandex.ru>
2025-07-06 01:51:14 +0300

    fix recvfrom

--- src/recvfrom.c
+++ src/recvfrom.c
@@ -3,13 +3,16 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <errno.h>
 #include "csock.h"
 
 _WCRTLINK int recvfrom(int s, void *buf, size_t len, int flags, struct sockaddr *from, socklen_t *fromlen)
 {
     ULONG32 recvlen;
-    int ret = ___csock_recv(s, buf, len, &recvlen);
+    struct sockaddr_in *from_sa = (struct sockaddr_in *) from;
+    int ret = ___csock_recvfrom(s, buf, len, &recvlen,
+            &from_sa->sin_addr.s_addr, &from_sa->sin_port);
     if (ret < 0) {
         errno = __csock_errno(ret);
         return -1;
