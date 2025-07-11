26a2763bbf4ed1bfb3cdf4ea52d4df23d74f7bf1
Stas Sergeev <stsp2@yandex.ru>
2025-07-12 01:11:51 +0300

    select: dont pass NULLs to asm impl
    asm stub doesn't have NULL checks.

--- src/select.c
+++ src/select.c
@@ -9,7 +9,18 @@ _WCRTLINK int select_s (int nfds, fd_set *readfds, fd_set *writefds,
               fd_set *exceptfds, struct timeval *timeout)
 {
     ULONG32 to = timeout->tv_sec * 1000000 + timeout->tv_usec;
-    int ret = ___csock_select(readfds, writefds, exceptfds, &to);
+    fd_set dummy;
+    int ret;
+
+    /* Don't pass NULL to our select() */
+    FD_ZERO(&dummy);
+    if (!readfds)
+        readfds = &dummy;
+    if (!writefds)
+        writefds = &dummy;
+    if (!exceptfds)
+        exceptfds = &dummy;
+    ret = ___csock_select(readfds, writefds, exceptfds, &to);
     if (ret < 0) {
         errno = __csock_errno (ret);
         return -1;
