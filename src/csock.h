94ccb2c9e614f5ab5a2ad4cd6779981734add8ef
Stas Sergeev <stsp2@yandex.ru>
2025-07-18 23:17:57 +0300

    add ioctlsocket()
    Supports FIONBIO and FIONREAD only for now.

--- src/csock.h
+++ src/csock.h
@@ -41,6 +41,7 @@ LONG32 __cdecl ___csock_getsockname(ULONG32 fd, __out ULONG32 *addr, __out unsig
 LONG32 __cdecl ___csock_getpeername(ULONG32 fd, __out ULONG32 *addr, __out unsigned short *port);
 LONG32 __cdecl ___csock_getnblkio(ULONG32 fd, __out ULONG32 *nb);
 LONG32 __cdecl ___csock_setnblkio(ULONG32 fd, ULONG32 nb);
+LONG32 __cdecl ___csock_fionread(ULONG32 fd, __out ULONG32 *outq);
 
 LONG32 __cdecl VxdGetEntry(ULONG32 id);
 int csock_init(void);
---
+++ src/ioctl.c
@@ -0,0 +1,26 @@
+/* socket lib by @stsp */
+/* Sybase Open Watcom Public License */
+
+#include <errno.h>
+#include <sys/ioctl.h>
+#include "csock.h"
+
+_WCRTLINK int ioctlsocket (int fd, int cmd, unsigned int *arg)
+{
+    int ret = -1;
+
+    switch (cmd) {
+    case FIONBIO:
+        ret = ___csock_setnblkio(fd, *arg);
+        break;
+    case FIONREAD:
+        ret = ___csock_fionread(fd, arg);
+        break;
+    default:
+        errno = EINVAL;
+        return ret;
+    }
+    if (ret < 0)
+        errno = __csock_errno(ret);
+    return ret;
+}
