7d52931a4d3a1ba4c7ac160e7b3b326525b714a9
Stas Sergeev <stsp2@yandex.ru>
2025-10-13 18:47:30 +0300

    log connect errors

--- src/connect.c
+++ src/connect.c
@@ -24,6 +24,7 @@ LDECL int CNV connect(SOCKET s, const struct sockaddr *serv_addr, socklen_t addr
             !psock[s].nb, psock[s].blk_arg);
     if (ret < 0) {
         errno = __csock_errno(ret);
+        DEBUG_STR("connect error %i\n", errno);
         return SOCKET_ERROR;
     }
     return ret;
